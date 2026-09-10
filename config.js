a54a5db389e72191c31a043ebead22627913ec54
Aryasstraa <agusary0408@gmail.com>
2026-09-11 02:44:13 +0800

    feat: update MONETAG_SMARTLINK_URL with new target link

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     API_BASE_URL: 'https://api.getfiles.my.id',
 
     // URL Smart Link Monetag (Masukkan direct link / smart link Monetag Anda di sini)
-    MONETAG_SMARTLINK_URL: '',
+    MONETAG_SMARTLINK_URL: 'https://omg10.com/4/11769062',
 
     // URL CDN Cloudflare R2 (Baru)
     R2_PUBLIC_URL: 'pub-1880d72cd0f343999d3197d949ff8213.r2.dev'
